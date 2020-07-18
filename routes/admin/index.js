2396cfd9b5afb5b5273c8f68560e18016032c5db
Alexis ARCHER <archer.alexis@hotmail.fr>
2020-07-18 09:11:22 +0200

    ok

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,14 +1,38 @@
 const express = require('express')
-const Index = express.Router()
-const models  = require('../../models');
+const fs = require('fs');
 const CRUD = require('express-sequelize-crud')
+const path = require('path')
+
+const models  = require('../../models');
+
+const Index = express.Router()
+
+const ModelsFolder = path.dirname(require.main.filename)+'/models/';
+const AllTables =  []
 
 
+    const getModels = new Promise ( (resolve, reject) => fs.readdir(ModelsFolder, function (err, files) {
+        //handling error
+        try {
+            files.forEach(function (file) {
+                //remove .js extension then push // first letter is uppercase and singular: Article
+                const fileTopush = file.slice(0, file.length-3)
+                fileTopush !== 'index' &&AllTables.push(fileTopush)
+            });
 
-
-Index.use(CRUD.crud('/articles', models.Article)) 
-Index.use(CRUD.crud('/contracts', models.Contract)) 
-
+            resolve()
+        } 
+        catch (error) {
+            console.log('error')
+        }       //listing all files using forEach
+        
+    })
+    )
+    
+    getModels.then((res)=>
+        AllTables.map(ModelName=>{Index.use(CRUD.crud(`/${ModelName}s`, models[ModelName]))})
+    )        
+                
 
 module.exports = Index
 
