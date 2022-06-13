e3d765ff74094e40459bc58849d0b9be33db9975
Alexis <alexis.archer44@gmail.com>
2022-06-13 17:20:07 +0200

    add seo forPages

---
+++ src/api/page-display/routes/page-display.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-display router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::page-display.page-display');
---
+++ src/api/page-display/services/page-display.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-display service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::page-display.page-display');
